ee963d38f4cb6ea094b4958a0699480ee6f6dfe2
adsime <adrianmelsom@gmail.com>
2018-08-30 18:59:39 +0200

    Added custom case manager template

--- src/utils/casemanager.py
+++ src/utils/casemanager.py
@@ -3,14 +3,6 @@ from src.utils.tflowtools import *
 
 class CaseManager:
 
-    def __init__(self,cfunc,vfrac=0,tfrac=0):
-        self.casefunc = cfunc
-        self.validation_fraction = vfrac
-        self.test_fraction = tfrac
-        self.training_fraction = 1 - (vfrac + tfrac)
-        self.generate_cases()
-        self.organize_cases()
-
     def generate_cases(self): pass
 
     def organize_cases(self): pass
@@ -23,6 +15,14 @@ class CaseManager:
 
 
 class DefaultCaseManager(CaseManager):
+
+    def __init__(self, cfunc, vfrac=0, tfrac=0):
+        self.casefunc = cfunc
+        self.validation_fraction = vfrac
+        self.test_fraction = tfrac
+        self.training_fraction = 1 - (vfrac + tfrac)
+        self.generate_cases()
+        self.organize_cases()
 
     def generate_cases(self):
         self.cases = self.casefunc()  # Run the case generator.  Case = [input-vector, target-vector]
@@ -43,4 +43,29 @@ class DefaultCaseManager(CaseManager):
     def get_testing_cases(self): return self.testing_cases
 
 
+class CustomCaseManager(CaseManager):
 
+    def __init__(self, file, vfrac=0, tfrac=0):
+        self.validation_fraction = vfrac
+        self.test_fraction = tfrac
+        self.training_fraction = 1 - (vfrac + tfrac)
+        self.generate_cases()
+        self.organize_cases()
+
+    def generate_cases(self):
+        pass
+
+    def organize_cases(self):
+        ca = np.array(self.cases)
+        np.random.shuffle(ca) # Randomly shuffle all cases
+        separator1 = round(len(self.cases) * self.training_fraction)
+        separator2 = separator1 + round(len(self.cases)*self.validation_fraction)
+        self.training_cases = ca[0:separator1]
+        self.validation_cases = ca[separator1:separator2]
+        self.testing_cases = ca[separator2:]
+
+    def get_training_cases(self): return self.training_cases
+
+    def get_validation_cases(self): return self.validation_cases
+
+    def get_testing_cases(self): return self.testing_cases
